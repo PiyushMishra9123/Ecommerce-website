56895da43d2e15dfde5f99b900ad2dd8f22dee9a
Piyush Mishra <pmishra70039@gmail.com>
2026-06-15 11:02:41 +0530

    Search + Pagination + Category Filter + Sorting

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -34,15 +34,54 @@ router.post("/", protect,isAdmin, async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const products = await Product.find()
+    const pageSize = 5
+    const page = Number(req.query.page) || 1
 
-    res.json(products);
+    const keyword = req.query.keyword ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
+        } : {};
+    const categoryFilter = req.query.category
+      ? {
+          category: req.query.category,
+        } : {}
+    let sortOption = {}
+
+    if (req.query.sort === "lowToHigh") {
+      sortOption = { price: 1 };
+    }
+
+    if (req.query.sort === "highToLow") {
+      sortOption = { price: -1 };
+    }
+
+    const filter = {
+      ...keyword,
+      ...categoryFilter,
+    }
+
+    const count = await Product.countDocuments(filter)
+
+    const products = await Product.find(filter)
+      .sort(sortOption)
+      .limit(pageSize)
+      .skip(pageSize * (page - 1));
+
+    res.json({
+      products,
+      page,
+      pages: Math.ceil(count / pageSize),
+      totalProducts: count,
+    })
   } catch (error) {
     res.status(500).json({
       message: error.message,
     })
   }
 })
+
 
 router.get("/:id", async (req, res) => {
   try {
