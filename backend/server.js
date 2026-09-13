1e4c4fda888ec449a23c0ad69876b7f0f2421dde
Piyush Mishra <pmishra70039@gmail.com>
2026-09-13 18:56:09 +0530

    Fix CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,17 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
